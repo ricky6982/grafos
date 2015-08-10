482c807cd231335ee3baceffa35138312ef549da
RickySSD <ricky6982@gmail.com>
2015-08-09 23:46:01 -0300

    Se agrego controles para la creación de nodos y arcos.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,1 +1,49 @@
 var app = angular.module('app',[]);
+
+app.controller('GrafoCtrl',[
+    '$scope',
+    function($scope){
+        $scope.nodes = new vis.DataSet([]);
+        $scope.edges = new vis.DataSet([]);
+        nodoId = 1;
+
+        container = document.getElementById('network');
+        data = {
+            nodes: $scope.nodes,
+            edges: $scope.edges
+        };
+        options = {};
+
+        network = new vis.Network(container, data, options);
+
+        $scope.nodo = {
+            add: function(tipo){
+                if (tipo == 'int') {
+                    color = '#CCCDC5';
+                    label = 'I-';
+                }else{
+                    color = '#F8D2AD';
+                    label = 'PR-';
+                }
+                nodo = {
+                    id: nodoId,
+                    label: label+nodoId,
+                    color: color
+                };
+                nodoId += 1;
+                $scope.nodes.add(nodo);
+            }
+        };
+
+        $scope.arco = {
+            add: function(){
+                arco = {
+                    from: $scope.arco_from,
+                    to: $scope.arco_to,
+                    label: $scope.arco_from+'-'+$scope.arco_to
+                };
+                $scope.edges.add(arco);
+            }
+        };
+    }
+]);
