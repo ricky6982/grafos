13cd2c3bcf35fa9ac39f040bbfd4080c4d780caf
RickySSD <ricky6982@gmail.com>
2015-08-17 01:36:57 -0300

    En desarrollo de la interfaz de usuario para los controles de usuario.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- app2/js/app.js
+++ app2/js/app.js
@@ -3,6 +3,34 @@ var app = angular.module('app', []);
 app.controller('AppCtrl',[
     '$scope',
     function($scope){
-        $scope.trayecto = [];
+        $scope.nodes = new vis.DataSet([]);
+        $scope.edges = new vis.DataSet([]);
+        nodoId = 1;
+
+        container = document.getElementById('network');
+        data = {
+            nodes: $scope.nodes,
+            edges: $scope.edges
+        };
+        options = {};
+
+        network = new vis.Network(container, data, options);
+
+        $scope.trayecto = [
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0},
+            {'id': 0}
+        ];
     }
 ]);
